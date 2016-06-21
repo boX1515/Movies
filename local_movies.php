9d1b2d6b30092235594f24fe8934de144b4bd1fe
Žan Pangršič <zan1515@gmail.com>
2016-06-21 22:36:28 +0200

    release candidate 0.1

--- local_movies.php
+++ local_movies.php
@@ -21,7 +21,10 @@ else
         <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ek+Mukta">
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
         <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.min.js"></script>
-        <script type="text/javascript" src="js/info.js"></script>
+        <script type="text/javascript" src="js/Mobile.js"></script>
+        <script type="text/javascript" src="js/localdb.js?n=1"></script>
+        <script type="text/javascript" src="js/search.js"></script>
+        
     </head>
     <body>
         <?php
@@ -38,43 +41,40 @@ else
             {
                odjava();
             }
-            /*if(isset($_GET['profil']))
-            {
-                profil();
-            }*/
+           
         ?>
         
         <div>
             <div class="nav-container-top f-nav">
                <div class="logo">
                     <img class="logo_img" src="assets/logo.png"/>
-                    <a href="mpage.php" class="logo_text">TheMovieDatabase</a>
+                    <a href="local_movies.php" class="logo_text">TheMovieDatabase</a>
                 </div>
                 <div id="wrap">
-                  <form action="" autocomplete="off">
-                     <input id="search" class="search2" name="search" onkeyup="getSearchedItem()" type="text" placeholder="Vnesite naslov filma" >
+                  <form id="form_search" action="" autocomplete="off">
+                     <input id="search" class="search2" onkeyup="getSearchedItem()" name="search" type="text" placeholder="Vnesite naslov filma..">
                       <input id="search_submit" class="search_submit" value="Rechercher" type="submit">
                   </form>
-                    <div id="movie_search_list"></div>
+                  <div id="movie_search_list"></div>
                 </div>
+               
                 <div class="nav">
                     <ul>
                         <li><a href="local_movies.php" class="links">DOMOV</a></li>
-                        <li><a href="info.php?odj=true" class="links">ODJAVA</a></li>
+                        <li><a href="local_movies.php?odj=true" class="links">ODJAVA</a></li>
+                        <!---<li><a href="local_movies.php" class="links">LOCAL DB</a></li>-->
                     </ul>
-                    
                     <!--Clear floating div -->
                     <div class="clear"></div> 
                 </div>
             </div>
         </div>
-        <div class="background_movie_image" id="background_movie"> </div>
-        <div id="movie_info" class="movie_top_wrapper">
-        
+        <div id="wrap_movie_list" class="wrap_movie_list_div">
+                    
         </div>
-    </body>
+        
     <footer>
-       <div>
+        <div>
             <a class="downloads" href="downloads/android/movieDb.apk">Android APK</a>
             <a class="zan">@ŽanPangršič</a>
             <a class="logedOn">Pozdravljen: <?php echo $user;?></a>
